286c5386f1c388d75dbd82df482c62c77fa0f5e6
ritulmanoj <63064189+ritulmanoj@users.noreply.github.com>
2020-10-19 22:50:44 +0530

    Update mbox1.py

--- mbox1.py
+++ mbox1.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  8 14:31:34 2020
-
-@author: Manoj kumar
-"""
-
 fname = input("Enter file name: ")
 if len(fname) < 1 :
     fname = "mbox-short.txt"
